f358100bd4d25ded2563da2380463437f1502f74
Peacefire <ganzorigenkhtsogt@gmail.com>
2023-01-08 12:27:20 +0800

    v.0.4

---
+++ react-project/src/components/cards.jsx
@@ -0,0 +1,59 @@
+import react from 'react'
+import cardData from '../data/card'
+import AliceCarousel from 'react-alice-carousel'
+import { Bootstrap } from 'react-bootstrap-icons'
+
+function Cards() {
+    const cardsData = cardData.map(dataEl => {
+        return (
+            <div className='container d-flex justify-content-around flex-wrap my-5'>
+                <div class="card m-1 w-50">
+                    <img id='cardsImg' src={dataEl.img} class="card-img-top" />
+                    <div class="card-body">
+                        <h5 class="card-title text-start">{dataEl.name}</h5>
+                        <p class="card-text text-start">{dataEl.price}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+        )
+    })
+    return (
+        <div>
+
+            < AliceCarousel className="container" disableButtonsControls="true" responsive={{
+                0: {
+                    items: 1,
+                },
+                1024: {
+                    items: 3,
+                    itemsFit: 'contain',
+                }
+            }} >
+                {cardsData}
+            </AliceCarousel>
+            < AliceCarousel className="container" disableButtonsControls="true" responsive={{ 1280: { items: 3 } }} >
+                {cardsData}
+            </AliceCarousel>
+        </div>
+    )
+}
+
+export default Cards
+
+// const crouselData1 = carouselData.map(el => {
+//     return (
+//         <div className="container d-flex justify-content-between sliderIMG">
+//             <div className="p-5">
+//                 <h2>{el.name}</h2>
+//                 <button className="btn btn-warning">Shop now</button>
+//                 <button className="btn btn-outline-secondary">View more</button>
+//             </div>
+//             <div className="d-flex align-items-center">
+//                 <img src={el.url} className={el.style} id="img" />
+//                 <p className="btn btn-warning rounded-5">{el.price}</p>
+//             </div>
+//         </div>
+//     )
+// })
