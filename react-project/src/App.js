c4c24120af13379b326f3350f3107e727eaa013d
Morning <morning@mstars-lab2-07s-iMac.local>
2023-01-10 12:32:00 +0800

    v.0.6

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -18,12 +18,79 @@ function App() {
       <Sliders2 />
       <Products />
       <Cards />
-      <div className="banner">
-        <input type="button" value="New laptop" />
-        <h1>Sale u to 50% off</h1>
-        <p>12 inch hd display</p>
-        <input type="button" value="Shop Now" />
+
+      {/* Banner Section  */}
+      <div className="container banner">
+        <div className='ban'>
+          <input type="button" value="New laptop" />
+          <h1>Sale u to 50% off</h1>
+          <p>12 inch hd display</p>
+          <input type="button" value="Shop Now" />
+
+        </div>
       </div>
+      {/* Banner Section  */}
+      {/* 3 Card Section  */}
+      <div className="row">
+        <div className="col-8 d-flex border rounded-4 m-1">
+          <img src="images/speaker1.png" alt="" />
+          <div className='big_card'>
+            <p>JBL bar 2.1 deep bass</p>
+            <p>$11,70</p>
+            <i>stars</i><br />
+            <button className='border-0 rounded-3 bg-info p-3'>Add to cart icon</button>
+          </div>
+        </div>
+        <div className="col mid_card">
+          <div className="border rounded-4 m-1 d-flex RCard" >
+            <img src="images/controller.png" alt="" />
+            <div>
+              <p>Play game</p>
+              <p>$11,70</p>
+              <i>stars</i>
+            </div><s></s>
+          </div>
+          <div className="border rounded-4 m-1 d-flex h-1 RCard">
+            <img src="images/speaker1.png" alt="" />
+            <div>
+              <p>Play game</p>
+              <p>$11,70</p>
+              <i>stars</i>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+      {/* 3 Card Section  */}
+
+      {/* Blue section  */}
+      <div className='bg-info row rounded-4 py-5 my-4'>
+        <div className='col-4 d-flex justify-content-center'>
+          <img className='m-0' src="images/box-tick.png" alt="" />
+          <div className='m-0'>
+            <h4>Free delivery</h4>
+            <p>on order above $50,00</p>
+          </div>
+        </div>
+        <div className='col-4 d-flex justify-content-center'>
+          <img className='m-0' src="images/crown.png" alt="" />
+          <div className='m-0'>
+
+            <h4>Best quality</h4>
+            <p>best quality in low price</p>
+          </div>
+        </div>
+        <div className='col-4 d-flex justify-content-center'>
+          <img className='m-0' src="images/Vector.png" alt="" />
+          <div className='m-0'>
+
+            <h4>1 year warranty</h4>
+            <p>Avaliable warranty</p>
+          </div>
+        </div>
+      </div>
+      {/* Blue section  */}
     </div>
   );
 }
--- react-project/src/data/card.js
+++ react-project/src/data/card.js
@@ -1,77 +1,115 @@
 const cardData = [
-    {   
-        id:1,
-        img:"images/camera.png",
-        name:"Camera",
-        price: "$11,70",
+    {
+        slider: 1,
+        children: [
+            {
+                id: 1,
+                img: "images/camera.png",
+                name: "Camera",
+                price: "$11,70",
+            },
+            {
+                id: 2,
+                img: "images/HeadPhone.png",
+                name: "Wirless headphone",
+                price: "$11,70",
+            },
+            {
+                id: 3,
+                img: "images/HeadPhone.png",
+                name: "Wirless headphone",
+                price: "$11,70",
+            },
+            {
+                id: 4,
+                img: "images/Controller.png",
+                name: "Play game",
+                price: "$11,70",
+            },
+            {
+                id: 5,
+                img: "images/Controller.png",
+                name: "Play game",
+                price: "$11,70",
+            },
+            {
+                id: 6,
+                img: "images/laptop.png",
+                name: "Tablet as laptop",
+                price: "$11,70",
+            },
+            {
+                id: 7,
+                img: "images/Controller.png",
+                name: "Play game",
+                price: "$11,70",
+            },
+            {
+                id: 8,
+                img: "images/laptop.png",
+                name: "Tablet as laptop",
+                price: "$11,70",
+            },
+        ]
     },
-    {   
-        id:2,
-        img:"images/HeadPhone.png",
-        name:"Wirless headphone",
-        price: "$11,70",
+    {
+        slider: 2,
+        children: [
+            {
+                id: 5,
+                img: "images/Controller.png",
+                name: "Play game",
+                price: "$11,70",
+            },
+            {
+                id: 6,
+                img: "images/laptop.png",
+                name: "Tablet as laptop",
+                price: "$11,70",
+            },
+            {
+                id: 7,
+                img: "images/Controller.png",
+                name: "Play game",
+                price: "$11,70",
+            },
+            {
+                id: 8,
+                img: "images/laptop.png",
+                name: "Tablet as laptop",
+                price: "$11,70",
+            },
+        ]
     },
-    {   
-        id:3,
-        img:"images/HeadPhone.png",
-        name:"Wirless headphone",
-        price: "$11,70",
-    },
-    {   
-        id:4,
-        img:"images/Controller.png",
-        name:"Play game",
-        price: "$11,70",
-    },
-    {   
-        id:5,
-        img:"images/Controller.png",
-        name:"Play game",
-        price: "$11,70",
-    },
-    {   
-        id:6,
-        img:"images/laptop.png",
-        name:"Tablet as laptop",
-        price: "$11,70",
-    },
-    {   
-        id:7,
-        img:"images/Controller.png",
-        name:"Play game",
-        price: "$11,70",
-    },
-    {   
-        id:8,
-        img:"images/laptop.png",
-        name:"Tablet as laptop",
-        price: "$11,70",
-    },
-    {   
-        id:9,
-        img:"images/camera1.png",
-        name:"Camera",
-        price: "$11,70",
-    },
-    {   
-        id:10,
-        img:"images/SaleLaptop.png",
-        name:"Laptop",
-        price: "$11,70",
-    },
-    {   
-        id:11,
-        img:"images/camera.png",
-        name:"Camera",
-        price: "$11,70",
-    },
-    {   
-        id:12,
-        img:"images/camera.png",
-        name:"Camera",
-        price: "$11,70",
-    },
-
+    {
+        slider: 2,
+        children: [
+            {
+                id: 9,
+                img: "images/camera1.png",
+                name: "Camera",
+                price: "$11,70",
+            },
+            {
+                id: 10,
+                img: "images/SaleLaptop.png",
+                name: "Laptop",
+                price: "$11,70",
+            },
+            {
+                id: 11,
+                img: "images/camera.png",
+                name: "Camera",
+                price: "$11,70",
+            },
+            {
+                id: 12,
+                img: "images/camera.png",
+                name: "Camera",
+                price: "$11,70",
+            },
+        ]
+    }
 ]
 
 export default cardData;
